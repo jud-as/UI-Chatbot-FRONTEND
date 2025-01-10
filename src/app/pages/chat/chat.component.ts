9ae2688239f6f4c2ff813c309d3fd7f130bac2c8
jud-as <guilhermevalente.dev@gmail.com>
2025-01-10 18:14:56 -0300

    chat component header

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import {ArrowLeftComponent} from '../../../icons/arrow-left/arrow-left.component';
+import {ElipseComponent} from '../../../icons/elipse/elipse.component';
+import {MapMuseumComponent} from '../../../icons/map-museum/map-museum.component';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-chat',
-  imports: [],
+  imports: [
+    ArrowLeftComponent,
+    ElipseComponent,
+    MapMuseumComponent
+  ],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
 export class ChatComponent {
+  constructor(private router: Router) {
+  }
 
+  openHome() {
+    this.router.navigate(['/']);
+  }
 }
