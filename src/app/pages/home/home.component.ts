05ecd53d70aca4dc1b3ebb5dd3c096b7f82b5abe
jud-as <guilhermevalente.dev@gmail.com>
2025-01-10 16:28:04 -0300

    feat: add initial logic to open chat

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {SparkleComponent} from '../../../icons/sparkle/sparkle.component';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,5 +11,10 @@ import {SparkleComponent} from '../../../icons/sparkle/sparkle.component';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  constructor(private router: Router) {
+  }
 
+  openChat() {
+    this.router.navigate(['/chat']);
+  }
 }
