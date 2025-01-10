d708ba20a255ee57dfc3f39541afa171c2d828e3
jud-as <guilhermevalente.dev@gmail.com>
2025-01-10 16:16:23 -0300

    feat: create home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {SparkleComponent} from '../../../icons/sparkle/sparkle.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    SparkleComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
