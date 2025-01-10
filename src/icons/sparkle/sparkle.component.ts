5c005eb47965169c0e62288a0de29de9d5eaf48d
jud-as <guilhermevalente.dev@gmail.com>
2025-01-10 16:16:39 -0300

    feat: create sparkle component

---
+++ src/icons/sparkle/sparkle.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'icon-sparkle',
+  imports: [],
+  templateUrl: './sparkle.component.html',
+})
+export class SparkleComponent {
+
+}
