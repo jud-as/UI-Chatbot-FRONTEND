36070f276d895995111e4b2e36620280a0cf9ecb
jud-as <guilhermevalente.dev@gmail.com>
2025-01-10 17:25:56 -0300

    feat: arrow right svg component added

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {SparkleComponent} from '../../../icons/sparkle/sparkle.component';
 import {Router} from '@angular/router';
+import {ArrowRightComponent} from '../../../icons/arrow-right/arrow-right.component';
 
 @Component({
   selector: 'app-home',
   imports: [
-    SparkleComponent
+    SparkleComponent,
+    ArrowRightComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
